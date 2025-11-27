ab5ecd0b6e534e749c156e3a09349c924d07ca5b
Ezedin Ahmed <ezedinadefa@gmail.com>
2025-11-27 15:42:57 +0300

    implementing loadbalancer and loadbalancer algorithm(round robin)

--- src/main/java/com/ezedin/loadBalancer/loadBalancer.java
+++ src/main/java/com/ezedin/loadBalancer/loadBalancer.java
@@ -1,4 +1,33 @@
 package com.ezedin.loadBalancer;
+import jakarta.servlet.http.HttpServlet;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+
+
 
 public class loadBalancer {
+    protected loadBalancer(int port){
+        var server = new Server(new QueuedThreadPool(20));
+        var connector = new ServerConnector(server);
+        connector.setPort(port);
+        server.addConnector(connector);
+
+
+        var context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        context.addServlet(new ServletHolder(new loadBalancerServlet(this)), "/");
+
+    }
+    public static class loadBalancerServlet extends HttpServlet {
+        private transient loadBalancer loadbalancer;
+
+        public loadBalancerServlet(loadBalancer loadBalancer) {
+            this.loadbalancer=loadBalancer;
+        }
+    }
 }
--- src/main/java/com/ezedin/loadBalancer/strategy/loadBalancerStrategy.java
+++ src/main/java/com/ezedin/loadBalancer/strategy/loadBalancerStrategy.java
@@ -1,4 +1,107 @@
 package com.ezedin.loadBalancer.strategy;
 
-public class loadBalancerStrategy {
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public abstract class loadBalancerStrategy {
+    private List<String> servers;
+    private List<String> validServers;
+    private List<String> invalidServers;
+
+    protected loadBalancerStrategy() {
+        this.servers = new ArrayList<>();
+        this.validServers = new ArrayList<>();
+        this.invalidServers = new ArrayList<>();
+    }
+    protected loadBalancerStrategy(List<String> list ) {
+        this.servers = new ArrayList<>();
+        this.validServers = new ArrayList<>();
+        this.invalidServers = new ArrayList<>();
+    }
+    public List<String> getServers() {
+        return new ArrayList<>(this.validServers);
+    }
+
+    public abstract Optional<String> getNext();
+    public abstract Optional<String> getCurrent();
+    public void startHealthCheck(){
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        
+        Runnable runnable = () -> {
+            var list = new ArrayList<String>(this.servers);
+            for (String server : list){
+                CompletableFuture.runAsync(() -> this.tryHealthCheck(server));
+            }
+        };
+        scheduler.scheduleAtFixedRate(runnable,0,2, TimeUnit.SECONDS);
+    }
+
+    public boolean tryHealthCheck(String url) {
+        if (this.isHealthy(url)) {
+            this.healthy(url);
+            return true;
+        } else {
+            this.unhealthy(url);
+            return false;
+        }
+    }
+
+    private void unhealthy(String url) {
+        if(!this.invalidServers.contains(url)) {
+            this.invalidServers.add(url);
+        }
+        this.validServers.remove(url);
+
+    }
+
+    private void healthy(String url) {
+        this.invalidServers.remove(url);
+        if(!this.validServers.contains(url)){
+            this.validServers.add(url);
+        }
+    }
+
+    private boolean isHealthy(String url) {
+        if(!isTcpHealthy(url)){
+            return false;
+        }
+        try{
+            var client = new OkHttpClient.Builder()
+                    .connectTimeout(1,TimeUnit.SECONDS)
+                    .readTimeout(3,TimeUnit.SECONDS)
+                    .build();
+            var request = new Request.Builder()
+                    .url(url + "/health")
+                    .get()
+                    .build();
+            var response = client.newCall(request).execute();
+            return response.code() == 200 && response.body().toString().contains("healthy");
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+    private boolean isTcpHealthy(String urlString) {
+        int timeout = 1000;
+        try(var socket = new Socket()){
+            var url = new URL(urlString);
+            socket.connect(new InetSocketAddress(url.getHost(),url.getPort()),timeout);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
 }
